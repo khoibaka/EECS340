b282cdc35f6801a0f103a1b27915e1a720d57b81
Khoi Le <khoibaka@gmail.com>
2020-04-18 15:11:22 -0400

    Add comments

--- utils/sql2python.py
+++ utils/sql2python.py
@@ -5,16 +5,19 @@ class SQL2PYTHON:
         self.__db_name = db_name
         self.__open_database(db_name)
 
+    #Helper to open back the database after closed
     def __open_database(self,db_name):
         self.__conn = sql.connect(db_name)
         self.__c = self.__conn.cursor()
         self.__closed = False
 
+    #Run the command from args
     def run(self, command):
         if(self.__closed):
             self.__open_database(self.__db_name)
         self.__c.execute(command)
 
+    #Close the database after used
     def close(self):
         self.__conn.commit()
         self.__c.close()
